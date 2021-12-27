7cc3a68cf74fc713fb15af7e6b3da39f66062e17
NyeonK <skdus728@gmail.com>
2021-12-27 19:02:11 +0900

    [baekjoon] 2501 약수 구하기

--- Baekjoon/2501.py
+++ Baekjoon/2501.py
@@ -1,4 +1,14 @@
 
+n, k = map(int, input().split())
 
+list = []
 
+for i in range(1, n+1):
+  if n % i == 0: 
+    # 약수
+    list.append(i)
 
+if len(list) < k:
+  print(0)
+else:
+  print(list[k-1])
