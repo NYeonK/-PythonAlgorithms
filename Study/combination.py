8b623ee01151a462aa8aa476d88612da4534897f
NyeonK <skdus728@gmail.com>
2022-01-17 02:14:24 +0900

    조합 nCk

--- Study/combination.py
+++ Study/combination.py
@@ -1,46 +1,59 @@
-# nCk
+# 조합 nCk
 
-# from itertools import combinations
+# itertools 사용한 조합
 
-# arr = [1,2,3,4,5]
-# for i in combinations(arr,3):
-#   print(i, end='')
+from itertools import combinations
+
+arr = [1,2,3,4,5]
+for i in combinations(arr,3):
+  print(i, end='')
 
 # ------------------------------------------------
 
 # 조합의 수 구하기
-# def combination(n,k):
-#   if k==0:
-#     return 1
-#   elif n<k:
-#     return 0
-#   else:
-#     return combination(n-1,k-1)+combination(n-1,k)
-# 
-# combination(5,3)
+
+def combination(n,k):
+  if k==0:
+    return 1
+  elif n<k:
+    return 0
+  else:
+    return combination(n-1,k-1)+combination(n-1,k)
+
+print(combination(5,3))
 
 # --------------------------------------------------
 
-def combination(arr,k):
-  result = []
+# 반복문을 사용한 조합
 
-  if len(arr)==k:
-    return arr
-  elif len(arr)<k or k==0:
-    return []
-  else:
-    for i in range(0, len(arr)):
-      center = arr[i]
-      next_arr = arr[i+1:]
-      
-      for j in combination(next_arr, k-1):
-        result.append([center]+j)
-        #k=0이 되는 순간,,이 옴
+A = [1,2,3,4,5]
 
-    return result
+def func(n):
+  for i in range(n-2):
+    for j in range(i+1, n-1):
+      for k in range(j+1, n): # i+2부터 범위를 잡게되면, 동일한 숫자가 포함될 수 있다.
+        print(A[i], A[j], A[k])
 
+func(len(A))
 
-arr = [1,2,3,4,5]
-print(combination(arr,3))
-# TypeError: 'int' object is not iterable
+#---------------------------------------------------
 
+# 재귀를 사용한 조합
+
+n=5
+k=3
+arr = [0]*k
+A=[1,2,3,4,5]
+
+def combi(level, S):
+  # 종료조건
+  if level >= k:
+    print(arr)
+    return
+
+# S : 시작점              
+  for i in range(S, n-k+level+1): # range를 사용하므로 +1   
+    arr[level] = A[i]
+    combi(level+1, i+1) # 다음 시작점(S)는 지금 숫자의 다음부터 진행해야 하므로 i+1
+
+combi(0,0)
