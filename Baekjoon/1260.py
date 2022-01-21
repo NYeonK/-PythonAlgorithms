75a375bf5a7d0667099dcce345dc5ad4466c9ccc
NyeonK <skdus728@gmail.com>
2022-01-21 23:50:50 +0900

    [baekjoon] 1260 DFS와 BFS

--- Baekjoon/1260.py
+++ Baekjoon/1260.py
@@ -1,11 +1,12 @@
 
 # DFS 선언
 def dfs(v): 
-  visited[v] = True # 방문 체크
+  visited[v] = True
   print(v, end=' ')
   for i in range(1, n+1):
     if not visited[i] and graph[v][i] == 1:
       dfs(i)
+
 
 
 # BFS 선언
@@ -24,19 +25,23 @@ def bfs(v):
         visited[i] = False
 
 
-# 입력: 정점의 개수 N, 간선의 개수 M, 탐색을 시작할 정점의 번호 V
-#       다음 M개의 줄에는 간선이 연결하는 두 정점의 번호 (입력으로 주어지는 간선은 양방향)
+
 import sys
 
 n, m, v = map(int, sys.stdin.readline().split())
+
 graph = [[0]*(n+1) for _ in range(n+1)]
 visited = [False] * (n + 1)
+
+# graph = [[0 0 0 0 0],[0 0 0 0 0],[0 0 0 0 0],[0 0 0 0 0],[0 0 0 0 0]]
+# visited = [False False False False False]
 
 
 for _ in range(m): 
     a, b = map(int, sys.stdin.readline().split())
     graph[a][b] = graph[b][a] = 1
 
+# graph = [[0 0 0 0 0],[0 0 1 1 1],[0 1 0 0 1],[0 1 0 0 1],[0 1 1 1 0]]
 
 dfs(v)
 print() 
